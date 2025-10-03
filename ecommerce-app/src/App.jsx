921e87e07e168b57aed7c2c318bf50dc88e77881
abrhot <abrihott@gmail.com>
2025-10-03 20:56:31 +0300

    first commit

---
+++ ecommerce-app/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-3xl font-bold">E-commerce App</h1>;
+}
+
+export default App;
---
+++ ecommerce-app/src/pages/ProductDetails.jsx
@@ -0,0 +1,5 @@
+function ProductDetails() {
+    return <h1 className="text-3xl font-bold">Product Details Page</h1>;
+}
+
+export default ProductDetails;
