0377f5ca82105e1c925d0bfd8c225213657ffe21
abrhot <abrihott@gmail.com>
2025-10-04 20:08:24 +0300

    update nav

--- ecommerce-app/src/components/Navbar.jsx
+++ ecommerce-app/src/components/Navbar.jsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between p-4 bg-gray-800 text-white">
-      <Link to="/">🏠 Home</Link>
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">
+        <Link to="/">EcommerceApp</Link>
+      </h1>
       <div className="flex gap-4">
-        <Link to="/products">🛍 Products</Link>
-        <Link to="/cart">🛒 Cart</Link>
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/products" className="hover:underline">Products</Link>
+        <Link to="/cart" className="hover:underline">Cart</Link>
       </div>
     </nav>
   );
